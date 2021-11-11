921d304995d40f64c35326624fb868588a5f87d0
Aswathy <iamaswathy884@gmail.com>
2021-11-11 13:29:36 +0530

    modified greatest number

--- javascript/greatest.js
+++ javascript/greatest.js
@@ -6,19 +6,16 @@
 // else{
 //     console.log(b,"is largest")
 // }
-function large(){
-    var a=5;
-    var b=8;
-    if(a>b)
-    {
-        console.log(a,"is large")
-    }
-    else if(a==b){
-        console.log("equal")
-    }
-    else{
-        console.log(b,"is large")
-    }
-}
-large()
-large()
+var a=5;
+var b=8;
+// if(a>b)
+//     {
+//         console.log(a,"is large")
+//     }
+//     else if(a==b){
+//         console.log("equal")
+//     }
+//     else{
+//         console.log(b,"is large")
+//     }
+console.log(a>b?a:b);
